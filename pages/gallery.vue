081dd4fa440cf4174f66a6f10564c00aa63ae902
p.psg <61703888+katsiree99@users.noreply.github.com>
2025-04-04 21:04:03 +0700

    updata imagesPerPage

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -258,7 +258,7 @@ const allImages = ref<any[]>([
 
 const nextPageToken = ref("");
 const page = ref(1);
-const imagesPerPage = 24;
+const imagesPerPage = 16;
 const isLoading = ref(false);
 const scrollTrigger = ref<HTMLElement | null>(null);
 
